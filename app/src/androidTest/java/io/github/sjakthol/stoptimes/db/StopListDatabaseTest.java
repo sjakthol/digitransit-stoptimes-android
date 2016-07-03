9071258377edd7cd149350919523b4a1c22afa07
Sami Jaktholm <sjakthol@outlook.com>
2016-07-03 09:53:37 +0300

    More db tests

--- app/src/androidTest/java/io/github/sjakthol/stoptimes/db/StopListDatabaseTest.java
+++ app/src/androidTest/java/io/github/sjakthol/stoptimes/db/StopListDatabaseTest.java
@@ -10,6 +10,8 @@ import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityTestCase;
 import android.test.InstrumentationTestCase;
 import io.github.sjakthol.stoptimes.db.task.GetFavoriteStopsTask;
+import io.github.sjakthol.stoptimes.db.task.QueryStopsTask;
+import io.github.sjakthol.stoptimes.db.task.UpdateFavoriteStatusTask;
 import io.github.sjakthol.stoptimes.utils.AsyncTaskResult;
 import io.github.sjakthol.stoptimes.utils.Logger;
 import org.json.JSONException;
@@ -20,6 +22,7 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.CountDownLatch;
 
+import static io.github.sjakthol.stoptimes.db.StopListContract.Stop.COLUMN_NAME_GTFS_ID;
 import static io.github.sjakthol.stoptimes.db.StopListContract.Stop.FAVORITES_TABLE_NAME;
 import static io.github.sjakthol.stoptimes.db.StopListContract.Stop.STOPS_TABLE_NAME;
 import static org.hamcrest.CoreMatchers.is;
@@ -48,7 +51,7 @@ public class StopListDatabaseTest extends InstrumentationTestCase {
             "        \"code\": \"Ki1518\",\n" +
             "        \"lon\": 24.375928,\n" +
             "        \"lat\": 60.201517,\n" +
-            "        \"name\": \"Solvik\",\n" +
+            "        \"name\": \"Kamppi 2\",\n" +
             "        \"gtfsId\": \"HSL:6150218\"\n" +
             "      },\n" +
             "      {\n" +
@@ -111,6 +114,107 @@ public class StopListDatabaseTest extends InstrumentationTestCase {
                 latch.countDown();
             }
         }.execute();
+
+        latch.await();
+    }
+
+    @Test
+    public void test_setFavoriteStatusToTrue() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final String stopId = "HSL:1040602";
+
+        new UpdateFavoriteStatusTask(mDbHelper) {
+            @Override
+            protected void onPostExecute(AsyncTaskResult<Void> result) {
+                super.onPostExecute(result);
+
+                assertThat("No errors occurred", result.getError(), is(not(notNullValue())));
+                assertThat("Task succeeded", result.isSuccess(), is(true));
+
+                Cursor res = mDbHelper.getReadableDatabase().rawQuery("SELECT * FROM " + FAVORITES_TABLE_NAME, null);
+                assertThat("One favorite found", res.getCount(), is(1));
+
+                assertThat(res.moveToFirst(), is(true));
+                assertThat("the favorite stop is correct", res.getString(0), is(stopId));
+
+                res.close();
+                latch.countDown();
+            }
+        }.execute(stopId, true);
+
+        latch.await();
+    }
+
+    @Test
+    public void test_removeFavoriteStatus() throws InterruptedException {
+        // Insert the stop into favorites
+        final String stopId = "HSL:1040602";
+        mDbHelper.getWritableDatabase()
+                 .execSQL(String.format("INSERT INTO %s VALUES('%s', 1)", FAVORITES_TABLE_NAME, stopId));
+
+        // Remove it using UpdateFavoritesTask
+        final CountDownLatch latch = new CountDownLatch(1);
+        new UpdateFavoriteStatusTask(mDbHelper) {
+            @Override
+            protected void onPostExecute(AsyncTaskResult<Void> result) {
+                super.onPostExecute(result);
+
+                assertThat("No errors occurred", result.getError(), is(not(notNullValue())));
+                assertThat("Task succeeded", result.isSuccess(), is(true));
+
+                Cursor res = mDbHelper.getReadableDatabase().rawQuery("SELECT * FROM " + FAVORITES_TABLE_NAME, null);
+                assertThat("No favorites left", res.getCount(), is(0));
+
+                res.close();
+                latch.countDown();
+            }
+        }.execute(stopId, false);
+
+        latch.await();
+    }
+
+    @Test
+    public void test_queryStopsTask() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        new QueryStopsTask(mDbHelper) {
+            @Override
+            protected void onPostExecute(AsyncTaskResult<Cursor> result) {
+                super.onPostExecute(result);
+
+                assertThat("No errors occurred", result.getError(), is(not(notNullValue())));
+                assertThat("Task succeeded", result.isSuccess(), is(true));
+
+                Cursor res = result.getResult();
+                assertThat("Got results", res, is(notNullValue()));
+                assertThat("Found two stops", res.getCount(), is(2));
+
+                res.close();
+                latch.countDown();
+            }
+        }.execute("kamppi", "20");
+
+        latch.await();
+    }
+
+    @Test
+    public void test_queryStopsTaskLimiting() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        new QueryStopsTask(mDbHelper) {
+            @Override
+            protected void onPostExecute(AsyncTaskResult<Cursor> result) {
+                super.onPostExecute(result);
+
+                assertThat("No errors occurred", result.getError(), is(not(notNullValue())));
+                assertThat("Task succeeded", result.isSuccess(), is(true));
+
+                Cursor res = result.getResult();
+                assertThat("Got results", res, is(notNullValue()));
+                assertThat("Only two stops returned", res.getCount(), is(2));
+
+                res.close();
+                latch.countDown();
+            }
+        }.execute("a", "2");
 
         latch.await();
     }
