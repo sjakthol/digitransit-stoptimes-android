99651853af27a6a738240a7a59a87e7324a3d051
Sami Jaktholm <sjakthol@outlook.com>
2016-07-17 10:52:36 +0300

    Convert UnexpectedErrorFragment to MessageFragment

--- app/src/main/java/io/github/sjakthol/stoptimes/activity/generic/UnexpectedErrorFragment.java
+++ app/src/main/java/io/github/sjakthol/stoptimes/activity/generic/UnexpectedErrorFragment.java
@@ -12,14 +12,14 @@ import io.github.sjakthol.stoptimes.R;
 /**
  * A {@link Fragment} for showing errors and exceptions to the user.
  */
-public class UnexpectedErrorFragment extends Fragment {
-    public UnexpectedErrorFragment() {}
+public class UnexpectedErrorFragment extends MessageFragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unexpected_error, container, false);
+    public UnexpectedErrorFragment() {
+        super();
+
+        setMessage(
+            R.string.unexpected_error_title,
+            R.string.unexpected_error_message
+        );
     }
-
 }
